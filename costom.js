c2658e9799f64c0121575857d61a93ecc0cd34d4
Achraf-dev <43117469+Achraf-dev@users.noreply.github.com>
2019-01-06 18:33:50 +0100

    Add files via upload

---
+++ costom.js
@@ -0,0 +1,13 @@
+/* global $, alert, console*/
+$(function(){
+
+'use strict';
+//trigger(ma3neha ykhadem) nice scroll pluggin
+  $('html').niceScroll();
+  
+
+});
+function sayHallo(){
+
+  alert("hallo html");
+}
